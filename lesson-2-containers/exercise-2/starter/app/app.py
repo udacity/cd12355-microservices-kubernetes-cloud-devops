3a71ed5283d41b9b068cfdab01e745117d31ca70
Justin Lee <hello@leejustin.com>
2023-02-19 11:18:26 +0800

    Configure additional database configs via environment variables

--- lesson-2-containers/exercise-2/starter/app/app.py
+++ lesson-2-containers/exercise-2/starter/app/app.py
@@ -8,9 +8,12 @@ from sqlalchemy import and_
 
 db_username = os.environ["DB_USERNAME"]
 db_password = os.environ["DB_PASSWORD"]
+db_host = os.environ.get("DB_HOST", "127.0.0.1")
+db_port = os.environ.get("DB_PORT", "5432")
+db_name = os.environ.get("DB_NAME", "postgres")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@127.0.0.1:5432/postgres"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 db = SQLAlchemy(app)
 
